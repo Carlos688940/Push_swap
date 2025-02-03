e175e63dbf97ee688bc8b2b628c97921b3b9efd9
Carlos688940 <carlosleal2000@gmail.com>
2025-02-03 22:53:11 +0000

    update

--- src/push_swap.c
+++ src/push_swap.c
@@ -68,36 +68,71 @@ void	find_price(t_stk_node *act, t_stk_node *dst, t_stk_node **cheapest)
 {
 	int	max_ind_act;
 	int	max_ind_dst;
+	int	mid_point;
 	t_stk_node	*ptr;
 
 	*cheapest = NULL;
 	ptr = act;
+	mid_point = ft_round(get_min(act)->value + get_max(act)->value) / 2;
 	max_ind_act = get_last(act)->index;
 	if (dst)
 		max_ind_dst = get_last(dst)->index;
 	while (ptr)
 	{
-		if (ptr->index <= max_ind_act / 2)
-			ptr->price = ptr->index;
-		else
-			ptr->price = (max_ind_act - ptr->index) + 1;
-		if (dst)
+		if (ptr->value <= mid_point)
 		{
-			if (dst->index <= max_ind_dst / 2)
-				ptr->price += dst->index;
+			if (ptr->index <= max_ind_act / 2)
+				ptr->price = ptr->index;
 			else
-				ptr->price += (max_ind_dst - dst->index) + 1;
+				ptr->price = (max_ind_act - ptr->index) + 1;
+			if (dst)
+			{
+				if (dst->index <= max_ind_dst / 2)
+					ptr->price += dst->index;
+				else
+					ptr->price += (max_ind_dst - dst->index) + 1;
 
-		}
-		if (!(*cheapest))
-			*cheapest = ptr;
-		else
-		{
-			if (ptr->price < (*cheapest)->price)
+			}	
+			if (!(*cheapest))
 				*cheapest = ptr;
-
+			else
+			{
+				if (ptr->price < (*cheapest)->price)
+					*cheapest = ptr;
+			}
 		}
 		ptr = ptr->next;
+	}
+}
+
+void	rotate_stks(t_stk_node **act, t_stk_node **dst, t_stk_node *cheapest)
+{
+	int	median_act;
+	int	median_dst;
+
+	median_act = get_last(*act)->index;
+	if (*dst)
+		median_dst = get_last(*dst)->index;
+	while ((*act != cheapest) && (*dst != cheapest->target))
+	{
+		if (cheapest->index <= median_act && cheapest->target->index <= median_dst)
+			rr(act, dst);
+		else
+			rrr(act, dst);
+	}
+	while (*act != cheapest)
+	{
+		if ((*act)->index <= median_act)
+			ra(act, 1);
+		else
+			rra(act, 1);
+	}
+	while (*dst != cheapest->target)
+	{
+		if ((*dst)->index <= median_dst)
+			rb(dst, 1);
+		else
+			rrb(dst, 1);
 	}
 }
 
@@ -111,6 +146,9 @@ void	push_to_b(t_stk_node **stk_a, t_stk_node **stk_b)
 	{
 		find_target(*stk_a, *stk_b, 'a');
 		find_price(*stk_a, *stk_b, &cheapest);
+		rotate_stks(stk_a, stk_b, cheapest);
+		pb(stk_a, stk_b);
+		max_ind = get_last(*stk_a)->index;
 	}
 }
 
@@ -119,5 +157,6 @@ void	ft_push_swap(t_stk_node **stk_a, t_stk_node **stk_b)
 	// t_stk_node	*cheapest;
 
 	push_to_b(stk_a, stk_b);
+	sort_three(stk_a);
 	sort_three(stk_a);
 }
